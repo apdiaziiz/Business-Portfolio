665bca580f9628925063f3cef5308297980a9ac2
apdiaziiz <suheyl260@gmail.com>
2023-06-18 07:56:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+
+// primitive datatype and reference datatype
+
+//primitive datatype
+
+// let fisrtName = 'suheyl'; // string literal
+// let number = 10; // number literL
+// let bool = true; // bool literal
+// let nothing = undefined; // undefinef
+// let empty = null; // null
+
+// console.log(typeof(fisrtName));
+// console.log(typeof(number));
+// console.log(typeof(bool));
+// console.log(typeof(nothing));
+// console.log(typeof(empty));
+
+// reference datatype
+
+// object reference literal
+
+// let person = 
+//     {name : 'suheyl',
+//     age : 20,
+//     address : {city : 'mogadisho',
+//     village: 'karan' },
+//     hoppies : 'reading mushaf'
+//     }
+
+// person.skills = 'it';
+// console.log(person)
+
+// array reference literal
+
+// let newArray = [];
+// newArray[0] = 'hello';
+
+// let someArray = [];
+// someArray[0] = 'world';
+
+
+// let adding = newArray.concat(someArray).join(' ');
+// console.log(adding);
+
+// console.log(newArray.indexOf('mango'));
+
+// events
+
+// let button = document.querySelector('.bg-light');
+// button.addEventListener('mouseover', buttonClick);
+// button.addEventListener('mouseout', buttonClick);
+// button.addEventListener('mouseenter', buttonClick);
+// button.addEventListener('mouseleave', buttonClick);
+// button.addEventListener('click', buttonClick);
+// button.addEventListener('dblclick', buttonClick);
+
+// function buttonClick (e){
+//     console.log('Event type is: ' + e.type);
+// }
+
+// functions 
+
+// function fname(hi,name,age){
+//     console.log(hi + ' my name is ' + name  +' and i am ' + age);
+// }
+// fname('hello','suheyl',20);
+
+// declaration function
+
+// let myname = function(great) {
+//     console.log(great + 'suheyl') ;
+// }
+// myname('hello ');
+
+// arrow function
+
+// let myname = (great) => {
+//     console.log(great + 'suheyl') ;
+// }
+// myname('hello');
+
+
+// loops
+
+// let list = 0;
+// while(list < 10){
+//     console.log('hello');
+//     list++;
+// }
+
+let last = document.querySelector('#last');
+last.classList = 'btn btn-danger btn-sm ml-3 delete';
+last.textContent = 'X';
+
+// let submit = document.querySelector('.btn-block');
+// submit.addEventListener('click', submitFunction);
+
+// function submitFunction(e) {
+//     e.preventDefault();
+//     names = document.querySelector('#name').value;
+//     phone = document.querySelector('#phone').value;
+//     address = document.querySelector('#address').value;
+// }
+
+
